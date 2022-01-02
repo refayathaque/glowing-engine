80a3ab2c132bcff33fb5fac276037c035ab87a9c
Refayat Haque <refayathaque@gmail.com>
2022-01-01 23:19:47 -0500

    back up

--- src/components/Cloud.js
+++ src/components/Cloud.js
@@ -2,6 +2,11 @@ const Cloud = () => {
   return (
     <div className="border border-primary">
       <h2>Cloud</h2>
+      <p>Cost</p>
+      <p>Reliability (99.9% SLAs, etc.)</p>
+      <p>
+        GCP for niche competitive advantage (check first chapter of Packt book)
+      </p>
     </div>
   );
 };
